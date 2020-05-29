937eedebf4acee0efd2d7a2501071c1162f9c4f2
zixinchu <66104925+zixinchu@users.noreply.github.com>
2020-05-29 12:49:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 褚梓鑫 2020年05月29日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
@@ -31,8 +31,8 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "音乐开始咯",
+        bannar_coming: "来点颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
